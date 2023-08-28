25b63679e399b172d0fe141dee765d01974cb60b
Crescent Geniius <crescent.sambila@gmail.com>
2023-08-28 16:17:25 +0300

    Modifying the Javascripts on Printing results

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,4 +1,4 @@
-<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+<a href="#" id="hide" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
 <!-- Vendor JS Files -->
 <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
